b274ebbde9c49aa2dd296b5a61af6abddf289dfb
carla-mulligan <S00186667@mail.itsligo.ie>
2020-05-08 00:26:12 +0100

    Git, Class, Xaml interface

---
+++ 8thMayRevision/BookInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8thMayRevision
+{
+    class BookInfo
+    {
+
+        
+
+         public string Title { get; set; }
+
+        public string YearPublished { get; set; }
+
+        public string Publisher { get; set;  }
+
+    }
+}
